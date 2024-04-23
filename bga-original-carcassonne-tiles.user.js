896b65070f311f88d87f8c1d83bec5dccd6394de
yzemaze <33390595+yzemaze@users.noreply.github.com>
2024-04-23 10:13:07 +0200

    Update bga-original-carcassonne-tiles.user.js
    + BGA in name

--- bga-original-carcassonne-tiles.user.js
+++ bga-original-carcassonne-tiles.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
-// @name         Original Carcassone Tiles
+// @name         BGA Original Carcassone Tiles
 // @namespace    https://github.com/yzemaze/bga-original-carcassonne-tiles/
-// @version      1.1
+// @version      1.2
 // @description  Displays the original carcassone tiles in boardgamearena.com when selecting the "Latest" "Artwork edition" from the playing menu.
 // @author       yzemaze
 // @match        https://boardgamearena.com/*
